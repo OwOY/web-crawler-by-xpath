943a0deb10a7f892bc41648465783950d5d339c0
OwOY <asd1234op@gmail.com>
2021-01-26 01:22:26 +0800

    update baha_xpath 2021/01/26

--- baha_xpath.py
+++ baha_xpath.py
@@ -4,180 +4,198 @@ import prettytable as pt
 import os
 
 
-
-class parse_baha:
-
-    tb = pt.PrettyTable(['ID','Category','TotalGBP','Title','Interactive','Hot','Last_comment'])
+class baha_gossiping:
 
     def __init__(self):
+
         self.requests = requests.Session()
+        self.main()
+
 
     def main(self):
+    
+        self.get_board_list(1)
+
+
+    def get_tree(self, url):
         
-        bsn = self.choose_board()
-        self.get_board_info(bsn)
+        resp = self.requests.get(url, headers = {
+            'Host': 'forum.gamer.com.tw',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0',
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Language': 'zh-TW,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+            'Accept-Encoding': 'gzip, deflate',
+            'X-Requested-With': 'XMLHttpRequest',
+            'DNT': '1',
+            'Connection': 'keep-alive',
+            'Referer': 'https://forum.gamer.com.tw/',
+        }).text
 
-    def choose_board(self):
+        html = etree.HTML(resp)
+        return html
 
-        gamer_choose_dict = {
-            '1':'場外哈拉版',
-            '2':'歡樂惡搞版',
-            '3':'Steam版',
-            '4':'職場甘苦談',
-            '5':'酸甜苦辣版'
-        }
-        gamer_Link_dict = {
-            '場外哈拉版':'60076',
-            '歡樂惡搞版':'60084',
-            'Steam版' : '60599',
-            '職場甘苦談':'60561',
-            '酸甜苦辣版':'60091'
-        }
+
+    def get_json(self, url, page):
+        
+        resp = self.requests.get(url, headers = {
+            'Host': 'forum.gamer.com.tw',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0',
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Language': 'zh-TW,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+            'Accept-Encoding': 'gzip, deflate',
+            'X-Requested-With': 'XMLHttpRequest',
+            'DNT': '1',
+            'Connection': 'keep-alive',
+            'Referer': 'https://forum.gamer.com.tw/',
+        },params = {
+            'c':'21',
+            'page':page
+        }).json()
+
+        return resp
+
+    
+    def get_board_list(self, page):
+        
+        id = 1
+        board = pt.PrettyTable(['ID', 'rank', 'board_name'])
+        resp = self.get_json('https://forum.gamer.com.tw/ajax/rank.php', page)
+        bsns = []
+        for data in resp:
+            board.add_row([id, data['ranking'], data['title']])
+            bsns.append(data['bsn'])
+            id += 1
+        print(board)
+        return self.choose_board(bsns, page)
+
+
+    def choose_board(self, bsns, page):
 
         while True:
-            choose_gamerindex = str(input('(0)離開(1)場外哈拉版(2)歡樂惡搞版(3)Steam版(4)職場甘苦談(5)酸甜苦辣版\n請選擇你要進入的版面 : '))
-            if choose_gamerindex == '0':
-                break
             try:
-                bsn = gamer_Link_dict[gamer_choose_dict[choose_gamerindex]]
+                user_choose = int(input('請選擇你的動作 (0)退出(1)進入版面(2)上一頁(3)下一頁 : '))
             except:
-                print('輸入錯誤請重請輸入')
-                os.system('pause')
+                print('請重新輸入')
                 continue
-        return bsn
-
-    def get_tree(self):
-        
-        resp = self.requests.get(
-                'https://forum.gamer.com.tw/B.php',
-                headers = {
-                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'accept-encoding': 'gzip, deflate',
-                'accept-language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-                'cache-control': 'max-age=0',
-                'referer': 'https://forum.gamer.com.tw/A.php?bsn=60076',
-                'sec-fetch-dest': 'document',
-                'sec-fetch-mode': 'navigate',
-                'sec-fetch-site': 'same-origin',
-                'sec-fetch-user': '?1',
-                'upgrade-insecure-requests': '1',
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
-            },
-                params = {
-                    'page' : page,
-                    'bsn': bsn
-                }
-            )
-
-        tree = etree.HTML(resp.text)
-        return tree
-
-    def get_category_list(self):
-
-        Category_List = []
-        Category = T.xpath('.//p[@class="b-list__summary__sort"]//text()')
-        [Category_List.append(Category[0])]
-        return Category_List
-    
-    def get_total_gbp(self):
-        
-        Total_GBP_List = []
-        Total_GBP = T.xpath('.//td[@class="b-list__summary"]//span//text()')
-        try:
-            Total_GBP_List.append(Total_GBP[0])
-        except:
-            Total_GBP_List.append('0')
-        return Total_GBP_List
-    def get_title_list(self):
-
-        Title_List = []
-        Title = T.xpath('.//div[@class="b-list__tile"]//p//text()')
-        [Title_List.append(Title[0])] 
-        return Title_List
-
-    def get_urls(self):
-
-        Link_List = []
-        Link = T.xpath('.//div[@class="b-list__tile"]//@href')
-        [Link_List.append('https://forum.gamer.com.tw/'+Link[0])]
-        return Link_List
-    def get_react(self):
-
-        Interactive_List = []
-        react = T.xpath('.//p[@class="b-list__count__number"]//text()')
-        [Interactive_List.append(react[1])]
-        return Interactive_List
-    
-    def get_hot_list(self):
-
-        Hot_List = []
-        [Hot_List.append(react[3])]
-        return Hot_List
-    
-    def get_last_comment(self):
-
-        Last_comment_List = []
-        Last_comment = T.xpath('.//p[@class="b-list__time__edittime"]//text()')
-        [Last_comment_List.append(Last_comment[1])]
-        return Last_comment_List
-
-    def get_board_info(self, bsn):
-
-        page  = 1
-        while True:
-        
-            tree = self.get_tree()
-            Total = tree.xpath('//tr[@class="b-list__row b-list-item b-imglist-item"]')
-            
-            for T in Total:
-                    
-            i = 1
-            for a,b,c,d,e,f in zip(self.get_category_list(), self.get_total_gbp(),\
-                                    self.get_title_list(),self.get_react(),\
-                                    self.get_hot_list(),self.get_last_comment()):
-                tb.add_row([i,a,b,c,d,e,f])
-                i+=1
-            print(tb)
-            user_choose = int(input('(0)重新選擇版面(1)進入頁面(2)下一頁(3)上一頁(4)取消\n請選擇想要的動作:'))
-            
             if user_choose == 0:
-                break
+                os._exit(0)
             elif user_choose == 1:
-                tb1 = pt.PrettyTable(['ID','comment'])
-                choose_index = int(input('請選擇想要查看的內文代碼：'))
-                p1 = requests.get(Link_List[choose_index-1],headers = h)
-                HTML = etree.HTML(p1.text)
-                post_time = HTML.xpath('//div[@class="c-post__header__info"]//a//@data-mtime')
-                comment = HTML.xpath('//article[@class="c-article FM-P2"]')
-                ID = 1
-                tb1 = pt.PrettyTable(['ID','comment'])
-                for C in comment:
-                    D = C.xpath('.//text()')
-                    while True:
-                        try:
-                            D.remove('\n')
-                        except:
-                            break
-                    if D == []:
-                        D.append('img')
-                    for E in D:
-                        if E == D[0]:
-                            tb1.add_row([ID, E.strip()])
-                        else:
-                            tb1.add_row(['',E.strip()])
-                    ID += 1
-                print(tb1)
-                os.system('pause')
+                try:
+                    choose_board = int(input('請選擇進入版面 :'))
+                except:
+                    print('請重新輸入')
+                    continue
+                self.get_article_list(bsns[choose_board-1], 1)
             elif user_choose == 2:
-                page +=1
-            elif user_choose == 3:
-                page -=1
-                if page ==0:
-                    print('已經是第一頁囉~')
+                if page == 1:
+                    print('已經是第1頁囉')
                     page = 1
-                    os.system('pause')
-            elif user_choose == 4:
-                continue
+                    # os.system('pause')   #windows選擇使用
+                    os.system('read -n 1 -p "Press any key to continue..."')    #Linux選擇使用
+                else:
+                    page -= 1
+                self.get_board_list(page)
+            elif user_choose == 3:
+                page += 1
+                self.get_board_list(page)
             else:
-                print('輸入錯誤，請重新輸入')
-                os.system('pause')
+                print('請重新輸入')
+
+
+    def get_article_list(self, bsn, page):
+        
+        id = 1
+        tb = pt.PrettyTable(['ID', 'category', 'hot', 'title' , 'react', 'view'])
+        html = self.get_tree(f'https://forum.gamer.com.tw/B.php?bsn={bsn}&page={page}')
+        category_list = html.xpath('//tr[@class="b-list__row b-list-item b-imglist-item"]/td[1]//p/a/text()')
+        hot_list = html.xpath('//tr[@class="b-list__row b-list-item b-imglist-item"]/td[1]')
+        hot_list = [hot.xpath('.//span/text()')[0] if hot.xpath('.//span/text()') != [] else '0' for hot in hot_list]
+        title_list = html.xpath('//tr[@class="b-list__row b-list-item b-imglist-item"]/td[2]//div[@class="b-list__tile"]/p/text()')
+        article_urls = html.xpath('//tr[@class="b-list__row b-list-item b-imglist-item"]/td[2]/a/@href')
+        react_list = html.xpath('//tr[@class="b-list__row b-list-item b-imglist-item"]/td[3]/p[1]/span[1]/text()')
+        view_list = html.xpath('//tr[@class="b-list__row b-list-item b-imglist-item"]/td[3]/p[1]/span[2]/text()')
+        for a, b, c, d, e in zip(category_list, hot_list, title_list, react_list, view_list):
+            tb.add_row([id, a, b, c, d, e])
+            id += 1
+        print(tb)
+        return self.choose_article(article_urls, bsn, page)
+
+
+    def choose_article(self, article_urls, bsn, page):
+        
+        while True:
+            try:
+                user_choose = int(input('請選擇你的動作 (0)離開(1)進入文章(2)上一頁(3)下一頁(4)重新選版 : '))
+            except:
+                print('請重新輸入')
+                continue
+            if user_choose == 0:
+                os._exit(0)
+            elif user_choose == 1:
+                try:
+                    choose_article = int(input('請選擇文章 : '))
+                except:
+                    print('請重新輸入')
+                    continue
+                self.get_article_content(f'https://forum.gamer.com.tw/{article_urls[choose_article-1]}', bsn)
+            elif user_choose == 2:
+                if page == 1:
+                    print('已經是第1頁囉')
+                    # os.system('pause')   #windows選擇使用
+                    os.system('read -n 1 -p "Press any key to continue..."')    #Linux選擇使用
+                else:
+                    page -= 1
+                    self.get_article_list(bsn, page)
+            elif user_choose == 3:
+                page += 1
+                self.get_article_list(bsn, page)
+            elif user_choose == 4:
+                self.get_board_list(1)
+            else:
+                print('輸入錯誤')
+    
+
+    def get_article_content(self, url, bsn):
+
+        page = 1
+        html = self.get_tree(url)
+        last_page = html.xpath('//p[@class="BH-pagebtnA"]/a[last()]/text()')
+
+        while True:
+            html = self.get_tree(f'{url}&page={page}')
+            tb = pt.PrettyTable(['User', 'Comment', 'Good', 'Bad'])
+            username_list = html.xpath('//a[@class="username"]/text()')
+            comment_list = html.xpath('//div[@class="c-article__content"]')
+            comment_list = [''.join(comment.xpath('.//text()')) for comment in comment_list]
+            gp_list = html.xpath('//div[@class="gp"]/a/text()')
+            bp_list = html.xpath('//div[@class="bp"]/a/text()')
+            for a, b, c, d in zip(username_list, comment_list, gp_list, bp_list):
+                tb.add_row([a, b, c, d])
+            print(tb)
+
+            user_choose = int(input('請選擇動作(0)退出(1)上一頁(2)下一頁(3)重新選擇文章 : '))
+            if user_choose == 0:
+                os._exit(0)
+            elif user_choose == 1:
+                if page == 1:
+                    print('已經是第1頁囉')
+                    # os.system('pause')   #windows選擇使用
+                    os.system('read -n 1 -p "Press any key to continue..."')    #Linux選擇使用
+                else:
+                    page -= 1
+            elif user_choose == 2:
+                if page == int(last_page[0]):
+                    print('已經最後一頁囉')    
+                    # os.system('pause')   #windows選擇使用
+                    os.system('read -n 1 -p "Press any key to continue..."')    #Linux選擇使用
+                else:
+                    page += 1
+            elif user_choose == 3:
+                self.get_article_list(bsn, 1)
+            else:
+                print('請重新輸入')
+
+if __name__ == '__main__':
+
+    baha_gossiping()
+    # baha_gossiping().get_article_list(23835)
